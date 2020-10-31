2a8c4d6ba3c7ff5ba6aba932b1813ef2762b6b0e
Aditeya Baral <aditeya.baral@gmail.com>
2020-10-31 10:15:41 +0530

    added final gui

--- src/gui.py
+++ src/gui.py
@@ -1,44 +1,55 @@
 import graph
 import scoring
+import utils
 from tkinter import *
 
 class App:
     def __init__(self):
         self.root = Tk()
         self.root.title("AutoCorrect")
-        self.root.configure(background="#121212")
-        #self.root.geometry("800x500")
+        self.root.configure(background="#d2d2c9")
+        self.root.geometry("800x900")
         self.keyAnswer = None
         self.inputAnswer = None
         self.marks = StringVar()
-        self.blank = Label(self.root, bg="#121212")
+        self.blank = Label(self.root, bg="#d2d2c9")
         self.blank.pack()
         self.welcome = Label(
-            self.root, text="Welcome to AutoCorrect!", background="#121212"
+            self.root, text="AutoCorrect", background="#d2d2c9"
         )
-        self.welcome.config(fg="#3b54ce", font=("Comfortaa", 40))
+        self.welcome.config(fg="#6d031c", font=("Comfortaa", 80))
         self.welcome.pack()
-        self.blank = Label(self.root, bg="#121212")
+        self.blank = Label(self.root, bg="#d2d2c9")
         self.blank.pack()
-        self.blank = Label(self.root, bg="#121212")
+        self.blank = Label(self.root, bg="#d2d2c9")
         self.blank.pack()
+        self.welcome = Label(
+            self.root, text="Key Answer", background="#d2d2c9"
+        )
+        self.welcome.config(fg="#6d031c", font=("Comfortaa", 30))
+        self.welcome.pack()
         self.key_answer_box = Text(self.root)
         self.key_answer_box.pack()
-        self.blank = Label(self.root, bg="#121212")
+        self.blank = Label(self.root, bg="#d2d2c9")
         self.blank.pack()
+        self.welcome = Label(
+            self.root, text="Test Answer", background="#d2d2c9"
+        )
+        self.welcome.config(fg="#6d031c", font=("Comfortaa", 30))
+        self.welcome.pack()
         self.input_answer_box = Text(self.root)
         self.input_answer_box.pack()
-        self.blank = Label(self.root, bg="#121212")
+        self.blank = Label(self.root, bg="#d2d2c9")
         self.blank.pack()
         self.grade_box = Message(
             self.root,
             textvariable=self.marks,
-            bg="#121212",
+            bg="#d2d2c9",
             font=("Calibri"),
             fg="white",
         )
         self.grade_box.pack()
-        self.blank = Label(self.root, bg="#121212")
+        self.blank = Label(self.root, bg="#d2d2c9")
         self.blank.pack()
         self.score_button = Button(
             self.root,
@@ -50,7 +61,7 @@ class App:
         )
         self.score_button.config(height=2, width=30, borderwidth=0)
         self.score_button.pack(side=TOP, expand=1)
-        self.blank = Label(self.root, bg="#121212")
+        self.blank = Label(self.root, bg="#d2d2c9")
         self.blank.pack()
 
         self.root.mainloop()
@@ -67,7 +78,7 @@ class App:
             key_answer_graph = graph.createGraph(self.keyAnswer.strip(), "key")
             input_answer_graph = graph.createGraph(self.inputAnswer.strip(), "ans")
             total_marks = scoring.evaluate(key_answer_graph, input_answer_graph, 4)
-            self.marks.set(str(total_marks))
-        
+            self.marks.set(f"Score: {utils.roundMarks(total_marks)}")
+
 
 App()
