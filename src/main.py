61cd482a3b0ca74d35d1a54def74372ce7a6e47a
aditeyabaral <aditeya.baral@gmail.com>
2021-08-15 12:22:48 +0530

    add auto load for model

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 import graph
 import scoring
 import utils
@@ -12,11 +13,18 @@ def readAnswer(filename):
     return content
 
 
-def loadModel(model_path):
-    '''
-    if model_path exists, load that model (detect automatically the type), else call createSentenceTransformerModel(model_path)
-    '''
-    pass
+def loadModel(model_path, model_type):
+    model_path = os.path.abspath(model_path)
+    if os.path.isfile(model_path):
+        if model_type == "fasttext":
+            model = embedding.loadFastTextModel(model_path)
+        else:
+            model = embedding.loadWord2VecModel(model_path)
+    elif os.path.isdir(model_path):
+        model = embedding.loadSentenceTransformerModel(model_path)
+    else:
+        model = embedding.createSentenceTransformerModel(model_path)
+    return model
 
 
 openie_client = utils.getOpenieClient()
@@ -25,12 +33,13 @@ parser = argparse.ArgumentParser(
 parser.add_argument("key_answer_filepath", metavar="KEY_ANSWER_FILEPATH")
 parser.add_argument("test_answer_filepath", metavar="TEST_ANSWER_FILEPATH")
 parser.add_argument("--model_path", metavar="MODEL_PATH")
+parser.add_argument("--model_type", metavar="MODEL_TYPE")
 parser.add_argument("--marks", metavar="TOTAL MARKS", default=10, type=float)
 args = vars(parser.parse_args())
 
 key_answer = readAnswer(args["key_answer_filepath"])
 test_answer = readAnswer(args["test_answer_filepath"])
-model = loadModel(args["model_path"])
+model = loadModel(args["model_path"], args["model_type"])
 total_marks = args["marks"]
 
 key_answer_graph = graph.createGraph(key_answer, "key", openie_client)
